7fb4043854bb85312d22e3a1f9d06b867602c709
Thammanit Rinthang <6431503031@lamduan.mfu.ac.th>
2025-02-04 14:41:31 +0700

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import "./globals.css";
 import Navbar  from "./components/navBar";
 
+export const metadata={
+  title: 'Process Deviation',
+  icons:{
+    icon:['/favicon.ico']
+  }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
